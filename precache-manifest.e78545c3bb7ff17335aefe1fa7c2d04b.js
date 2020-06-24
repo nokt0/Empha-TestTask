39cfb6b66155fe9e4b6c9a03bac3dfcd81200700
vladislav <vlad60870@gmail.com>
2020-06-24 20:05:58 +0300

    Updates

---
+++ precache-manifest.e78545c3bb7ff17335aefe1fa7c2d04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fc54f6d83092131ba46644b841aedf",
+    "url": "/nokt0/Empha-TestTask/index.html"
+  },
+  {
+    "revision": "0628efef575f3cb5bb2e",
+    "url": "/nokt0/Empha-TestTask/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "827ae8d341930f4bc8a4",
+    "url": "/nokt0/Empha-TestTask/static/css/main.8917f35e.chunk.css"
+  },
+  {
+    "revision": "0628efef575f3cb5bb2e",
+    "url": "/nokt0/Empha-TestTask/static/js/2.824ec24b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nokt0/Empha-TestTask/static/js/2.824ec24b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827ae8d341930f4bc8a4",
+    "url": "/nokt0/Empha-TestTask/static/js/main.d81e91f7.chunk.js"
+  },
+  {
+    "revision": "4369c5c70a646ed5863b",
+    "url": "/nokt0/Empha-TestTask/static/js/runtime-main.73db4181.js"
+  }
+]);
